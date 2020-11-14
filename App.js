a43382867308665c4ca42ba70ef7fe15c9c8a640
Samuel Kupoe <samuelkupoe1@gmail.com>
2020-11-14 13:32:59 +0000

    Form Created

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username:"",
+      Password:""
+    
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        
+      <View>
+      <Text style={styles.loginText} >Log in</Text>
+      </View>
+       
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.input}
+      placeholder="Username"
+      autoCapitalize="none"
+      autoCorrect={false}
+      value={this.state.email}
+      onChangeText={(email)=>{
+        this.setState({email})
+      }}
+      />
+
+      <TextInput style={styles.input}
+      placeholder="Password"
+      autoCapitalize="none"
+      autoCorrect={false}
+      secureTextEntry={true}
+      value={this.state.password}
+      onChangeText={(password)=>{
+        this.setState({password})
+      }}
+      />
+      <Text style={styles.forgotPassword}>Forgot password?</Text>
     </View>
-  );
+        
+    <View>
+        <TouchableOpacity style={styles.opacity}>
+           <Text style={styles.opacityText}>Log in</Text>
+        </TouchableOpacity>
+    </View>
+
+    <View style={styles.accountContainer}>
+      <Text style={styles.accountText}>Don't have account?</Text>
+      <Text style={styles.signUp}>Sign up</Text>
+    </View>
+        
+        
+      </View>
+    ); 
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:80,
+    marginHorizontal:70,
+    
   },
+  loginText: {
+    fontSize:40,
+    color:"#5669cc",
+    marginVertical:20
+  },
+  
+  input: {
+    borderBottomWidth:2,
+    borderColor:"#5669cc",
+    marginTop:60,
+    fontSize:20,
+    paddingBottom:5,
+    
+  },
+  forgotPassword:{
+    color:"#5fcaed",
+    alignSelf:"flex-end",
+    paddingTop:8
+  },
+  opacity: {
+    backgroundColor:"#5669cc",
+    width: 200,
+    height:40,
+    alignSelf:"center",
+    borderRadius:10,
+    marginVertical:40
+  },
+  opacityText: {
+    color:"white",
+    fontSize:25,
+    alignSelf:"center",
+    paddingVertical:2
+
+  },
+  accountContainer:{
+   flexDirection:"row",
+   alignSelf:"center",
+  },
+  accountText:{
+    fontSize:18,
+  },
+  signUp:{
+    fontSize:18,
+    color:"#5669cc",
+    marginLeft:5
+  }
 });
+
+export default App;
